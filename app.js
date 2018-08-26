f75464758727b1b8e38b56fc59d431d37a612d91
Varun Ramachandran <varun.rama@gmail.com>
2018-08-26 01:22:54 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+
+// EXPRESS JS PORT
+var port = process.env.PORT || 3000;
+
+// START SERVER
+server.listen(port, function () {
+    console.log("Server running on port: " + port.toString())
+});
+
+app.get("/", function(req, res) {
+    res.send("THIS WORKS");
+});
